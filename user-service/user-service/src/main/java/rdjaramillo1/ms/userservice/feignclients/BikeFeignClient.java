5d601bc6e88eac1ce4bb8e3e5a225f798a51f055
renzodamian <renzojaramillocueva@gmail.com>
2022-02-09 16:43:42 -0500

    update eureka

--- user-service/user-service/src/main/java/rdjaramillo1/ms/userservice/feignclients/BikeFeignClient.java
+++ user-service/user-service/src/main/java/rdjaramillo1/ms/userservice/feignclients/BikeFeignClient.java
@@ -1,16 +1,12 @@
 package rdjaramillo1.ms.userservice.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import rdjaramillo1.ms.userservice.model.Bike;
 
 import java.util.List;
 
 @FeignClient(name = "bike-service", url = "http://localhost:8003/bike")
-//@RequestMapping("/bike")
 public interface BikeFeignClient {
 
     @PostMapping()
--- user-service/user-service/src/main/java/rdjaramillo1/ms/userservice/feignclients/CarFeignClient.java
+++ user-service/user-service/src/main/java/rdjaramillo1/ms/userservice/feignclients/CarFeignClient.java
@@ -1,16 +1,12 @@
 package rdjaramillo1.ms.userservice.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import rdjaramillo1.ms.userservice.model.Car;
 
 import java.util.List;
 
 @FeignClient(name = "car-service", url = "http://localhost:8002/car")
-//@RequestMapping("/car")
 public interface CarFeignClient {
 
     @PostMapping()

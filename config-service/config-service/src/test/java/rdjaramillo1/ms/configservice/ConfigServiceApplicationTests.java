963d9933eac225c435147c2039db2e4f6ce44cbd
renzodamian <renzojaramillocueva@gmail.com>
2022-02-08 21:10:31 -0500

    config server

---
+++ config-service/config-service/src/test/java/rdjaramillo1/ms/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rdjaramillo1.ms.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

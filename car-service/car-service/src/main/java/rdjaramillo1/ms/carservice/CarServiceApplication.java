4db9fe7104a2af204488bd58e4467385cf3a778d
renzodamian <renzojaramillocueva@gmail.com>
2022-02-08 23:44:46 -0500

    eureka

--- car-service/car-service/src/main/java/rdjaramillo1/ms/carservice/CarServiceApplication.java
+++ car-service/car-service/src/main/java/rdjaramillo1/ms/carservice/CarServiceApplication.java
@@ -2,8 +2,10 @@ package rdjaramillo1.ms.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarServiceApplication {
 
 	public static void main(String[] args) {

5eb928b433d2c7aa01408370120a3083881cdb3c
renzodamian <renzojaramillocueva@gmail.com>
2022-02-10 16:29:37 -0500

    gateway

---
+++ gateway-service/gateway-service/src/test/java/rdjaramillo1/ms/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rdjaramillo1.ms.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
